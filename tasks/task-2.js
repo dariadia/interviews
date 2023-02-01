cb81f3f07ae531b349c5d6d12ca97e08a094cd85
Daria <daria.diachkova@mail.ru>
2023-02-01 17:28:02 +0100

    feat(app): add tasks

---
+++ tasks/task-2.js
@@ -0,0 +1,17 @@
+// Find errors and name their types:
+
+// 1
+const someMethod = () => {
+  console.log(counter1)
+  console.log(counter2)
+  var counter1 = 1
+  let counter2 = 2
+}
+
+// 2
+let someThing = {
+  action: 'I do something'
+}
+someThing.action()
+
+// 3
